aa3a814d499532d6eca27ce1c5a384482856f95c
ctbarbella <ctbarbella@loyola.edu>
2020-01-30 17:01:11 -0500

    helo.

---
+++ lab 02 goodson-leimbach.py
@@ -0,0 +1,28 @@
+# Programmers: Christopher Goodson - Eddie Leimbach
+# Course: CS151, Dr. Simari
+# Date: Thu Jan 30
+# Lab: 2
+# Program Inputs: births, deaths and migrations per second, current population, and the desired number of years in future for the estimate
+# Program Outputs: future populaton size for a country given its number of births, deaths, and migrations per second
+
+ # output purpose
+
+ print("This program will estimate future population size for a country given its number of births, deaths, and migrations per second, the current population, and the desired number of years in the future for the estimate")
+
+ # ask user for an estimate of births per second
+
+births per second = input("Please enter the birth rate per second")
+
+ # ask user for an estimate of deaths per second
+
+ deaths per second = input("Please enter the death rate per second")
+
+ # ask user for an estimate of the  migration per seconds
+
+ migration per second = input("Please ")
+
+ # ask user for an estimate of the current population
+
+ # ask user how many years in advance for population in years to come
+
+ # ask user for an estimate of future population
